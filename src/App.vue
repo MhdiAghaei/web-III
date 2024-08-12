f4aec235cc33134d40d4bfb96c570b2111fe1782
MhdiAghaei <147084529+MhdiAghaei@users.noreply.github.com>
2024-08-12 16:57:38 +0330

    Add files via upload
    i got some errors in this Codes can you help me????

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <followerList :followers="followers" />
+  </div>
+</template>
+
+<script>
+import followerList from "./components/list.vue";
+export default {
+  name: "App",
+  components: {
+    followerList,
+  },
+  data() {
+    return {
+      followers: [
+        { id: 11, fname: "mahdi" },
+        { id: 12, fname: "reza" },
+        { id: 13, fname: "abbas" },
+        { id: 14, fname: "ali" },
+        { id: 15, fname: "mamad" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li :v-for="follower in followers" :key="follower.id">
+        <p>{{ follower.name }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "followerList",
+  props: ["followers"],
+};
+</script>
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
